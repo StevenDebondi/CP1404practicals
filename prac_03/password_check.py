e4fe500944fb896308f487f810af4b5c2eeafdb1
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-08-22 10:51:35 +1000

    Refactor get_password function

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,20 @@
 """Password Checker"""
 
-password = input("Password: ")
-password_length = 6
 
-while len(password) < password_length:
-    print("Invalid Length")
+def main():
+    """"""
+    password = get_password()
+    password_length = 6
+
+    while len(password) < password_length:
+        print("Invalid Length")
+        password = get_password()
+    print("*" * len(password))  # Prints asterisks to match the amount of characters in the given password
+
+
+def get_password():
     password = input("Password: ")
-print("*" * len(password))  # Prints asterisks to match the amount of characters in the given password
+    return password
+
+
+main()
