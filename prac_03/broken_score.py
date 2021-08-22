ade06198fb9052f054c39f33a937b4aca571a6bd
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-08-22 11:17:59 +1000

    Modified broken_score.py to use functions and return score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,14 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Gets the score from the user and prints the results"""
+    score = float(input("Enter score: "))
+    print(get_result(score))
+
+
+def get_result(score):
+    """Gets the result of the given score"""
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
