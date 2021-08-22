10352307811c442f527eeaace487d92d4668d78f
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-08-22 10:53:01 +1000

    Refactor printing asterisks

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -9,10 +9,15 @@ def main():
     while len(password) < password_length:
         print("Invalid Length")
         password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
     print("*" * len(password))  # Prints asterisks to match the amount of characters in the given password
 
 
 def get_password():
+    """Gets the password from the user"""
     password = input("Password: ")
     return password
 
