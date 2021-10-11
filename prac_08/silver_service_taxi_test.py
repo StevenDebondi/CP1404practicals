bd7cf0ccb6d088677ff0e9357dd600639229be99
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-10-11 11:56:05 +1000

    Add rounding to taxi.py get_fare function and added formatting for displaying cents correctly in silver_service_taxi_test.py

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -10,7 +10,7 @@ def main():
     """Testing the SilverServiceTaxi Class"""
     my_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
     my_taxi.drive(18)
-    print(my_taxi, f"for a total of ${my_taxi.get_fare()}")
+    print(my_taxi, f"for a total of ${my_taxi.get_fare():.2f}")
 
 
 main()
