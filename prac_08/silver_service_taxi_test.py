15d9cce752e46be0262351578b6ae943f910fff8
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-10-11 11:51:27 +1000

    Add silver_service_taxi.py specialized Taxi and silver_service_taxi_test.py to test

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical
+Silver Service Taxi
+"""
+
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Testing the SilverServiceTaxi Class"""
+    my_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
+    my_taxi.drive(18)
+    print(my_taxi, f"for a total of ${my_taxi.get_fare()}")
+
+
+main()
