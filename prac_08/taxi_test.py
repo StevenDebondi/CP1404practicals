6140e46e5e1549a4a272e7f9203311016dd6fb0c
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-10-11 11:12:20 +1000

    Change taxi_test.py to use a constant for price_per_km and revised taxi_test.py to match

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -7,7 +7,7 @@ from taxi import Taxi
 
 def main():
     """Testing the Taxi Class"""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi, my_taxi.get_fare())
     my_taxi.start_fare()
