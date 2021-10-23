0f587b7c6fccb474c7732a309dff10fe559c3d7c
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-10-23 14:32:26 +1000

    Cleaned up cleanup_files.py for figuring out the renaming

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -9,40 +9,30 @@ import os
 def main():
     """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
-    os.chdir('Lyrics/Christmas')
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        pass
-    for filename in os.listdir('.'):
-        if os.path.isdir(filename):
-            continue
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-
-def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
-
-
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
         for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+
             name_name = os.path.join(directory_name, filename)
             print(name_name)
             new_name = os.path.join(directory_name, get_fixed_filename(filename))
             os.rename(name_name, new_name)
 
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    for i, character in enumerate(filename):
+        if character.islower():
+
+
+
+    print(filename)
+
+
+    return new_name
 
 main()
 # demo_walk()
