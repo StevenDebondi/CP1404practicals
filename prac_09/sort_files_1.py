1f127f5e55196ec6d395a719e39703bff93e51df
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-10-23 17:28:19 +1000

    Add sort_files_1.py using os.mkdir and shutil.move

---
+++ prac_09/sort_files_1.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical
+Sorting Files Version 1
+"""
+import os
+import shutil
+
+
+def main():
+    """Move files into folders based on their extensions"""
+    os.chdir('FilesToSort')
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+    for filename in os.listdir('.'):
+        extension = filename.split('.')[-1]
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+        shutil.move(filename, "{}/{}".format(extension, filename))
+
+
+main()
