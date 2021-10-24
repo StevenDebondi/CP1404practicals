2d342e1cccccc54a878146c5b115c3fcf1407852
Steven Debondi <Steven.debondi@my.jcu.edu.au>
2021-10-24 17:34:53 +1000

    Add fixing filenames for spaces into underscores

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -3,18 +3,17 @@ CP1404/CP5632 Practical
 Cleanup Files
 """
 import shutil
-import os
+import os, os.path
 
 
 def main():
-    """Demo os module functions."""
+    """Clean up files to be consistent naming"""
     print("Starting directory is: {}".format(os.getcwd()))
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
         for filename in filenames:
             new_name = get_fixed_filename(filename)
             print("Renaming {} to {}".format(filename, new_name))
-
             name_name = os.path.join(directory_name, filename)
             print(name_name)
             new_name = os.path.join(directory_name, get_fixed_filename(filename))
@@ -23,16 +22,10 @@ def main():
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    for i, character in enumerate(filename):
-        if character.islower():
-
-
-
-    print(filename)
-
-
+    spaced_name = filename.split(" ")
+    print(spaced_name)
+    new_name = "_".join(spaced_name)
     return new_name
 
+
 main()
-# demo_walk()
